e3b1a1e07812b90a7561b9a4b8bef0dfae74f81d
Serge Savel <serge@savel.pro>
2020-04-15 14:06:26 +0400

    Обновлен Copyright.

--- src/Platform/Session.cs
+++ src/Platform/Session.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 Serge Savelev
+﻿// Copyright 2020 Sergey Savelev
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
